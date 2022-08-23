df95ebf9d0b8625c99e23f514a98a727a533186c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-08-23 14:21:02 -0500

    fix: Fixed login form.

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -1,12 +1,23 @@
-import {redirect} from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import type { ActionFunction, MetaFunction } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+
+export const action: ActionFunction = async ({ request }) => {
+  // TODO (Josh - 8/23/22): Handle login here!
+  return redirect(`/`);
+};
+
+export const meta: MetaFunction = () => {
+  return { title: `IRIDA Login Page` };
+};
 
 export default function Login() {
   return (
-      <div>
-          <form>
-              <h1>Welcome to IRIDA Next UI</h1>
-              <button type="submit">Login</button>
-          </form>
-      </div>
+    <div>
+      <Form method="post">
+        <h1>Welcome to IRIDA Next UI</h1>
+        <button type="submit">Login</button>
+      </Form>
+    </div>
   );
 }
